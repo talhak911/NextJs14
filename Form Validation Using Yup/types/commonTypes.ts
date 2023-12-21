4b01899eb47b877dad7133ecc038ce566eeaed7e
talhak911 <talhakhan8300@gmail.com>
2023-12-21 21:25:46 +0500

    Form validation

---
+++ Form Validation Using Yup/types/commonTypes.ts
@@ -0,0 +1,15 @@
+type inputType = {
+    firstname: string,
+    lastname: string,
+    gender: string,
+    fname: string,
+    country: string,
+    address: string,
+    phone: number,
+    email: string,
+    subject: string,
+    message: string,
+}
+type onChangeEventTypes ={
+    target: { value: string, name:string } 
+}
