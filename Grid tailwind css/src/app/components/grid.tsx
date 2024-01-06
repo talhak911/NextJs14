465a28b3f4996faeb4c14e59b5d0147a1b243091
talhak911 <talhakhan8300@gmail.com>
2024-01-06 20:11:14 +0500

    Grid layout

--- Grid tailwind css/src/app/components/grid.tsx
+++ Grid tailwind css/src/app/components/grid.tsx
@@ -1,27 +1,48 @@
+import Image from "next/image";
 
-import Image from "next/image"
 export default function Grid() {
   return (
-    <div className="grid lg:grid-cols-3 lg:grid-rows-3 gap-4 m-8 md:grid-cols-2 md:grid-rows-2 ">
-      <div className="bg-gray-400 col-span-2 rounded-lg border-2 border-black text-3xl font-sans text-center h-[350px] grid grid-cols-4">
-        <img 
-          className="  h-full"
+    <div className="grid grid-cols-1 md:grid-cols-3 md:grid-rows-3 gap-4 m-8 sm:grid-cols-1 md:px-[250px]">
+      <div className="sm:col-span-1 md:col-span-2 h-[150px]">
+        <img
+          className="h-full w-full object-cover"
           src="https://mobisoftinfotech.com/resources/wp-content/uploads/2022/04/next-JS-framework.png"
           alt="Image 1"
         />
+        1
       </div>
-      <div className="bg-gray-400 row-span-2 rounded-lg border-2 border-black text-3xl font-sans text-center h-[700px]">
+      <div className="sm:col-span-1 md:row-span-2 h-[320px]">
+        <img
+          className="h-full w-full object-cover"
+          src="https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?cs=srgb&dl=pexels-junior-teixeira-2047905.jpg&fm=jpg"
+          alt="Image 2"
+        />
         2
       </div>
-      <div className="bg-gray-400 row-span-2 col-span-1 rounded-lg border-2 border-black text-3xl font-sans text-center h-[700px]">
+      <div className="sm:col-span-1 md:row-span-2 h-[320px]">
+        <img
+          className="h-full w-full object-cover"
+          src="https://cdn.thenewstack.io/media/2023/12/4e01e7fa-pexels-sanket-mishra-16380906-1024x683.jpg"
+          alt="Image 3"
+        />
         3
       </div>
-      <div className="bg-gray-400 rounded-lg col-span-1 border-2 border-black text-3xl font-sans text-center h-[350px]">
+      <div className="sm:col-span-1  h-[150px]">
+        <img
+          className="h-full w-full object-cover"
+          src="https://www.syncfusion.com/blogs/wp-content/uploads/2022/09/Optimize-NextJS-App-bundle-performance.png"
+          alt="Image 4"
+        />
         4
       </div>
-      <div className="bg-gray-400 rounded-lg border-2 col-span-2 border-black text-3xl font-sans text-center h-[350px]">
+      <div className="sm:col-span-1 md:col-span-2 h-[150px]">
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/1*GgubNcjyyNyfTuOS_ub1Rw.png"
+          alt="Image 5"
+        />
         5
       </div>
     </div>
   );
-};
+}
--- Grid tailwind css/src/app/page.tsx
+++ Grid tailwind css/src/app/page.tsx
@@ -2,7 +2,7 @@ import Grid from "./components/grid";
 
 export default function Home() {
   return (
-    <div className="container mx-auto mt-8">
+    <div>
     <h1 className="text-3xl font-bold mb-4">Image Grid</h1>
     <Grid />
   </div>
