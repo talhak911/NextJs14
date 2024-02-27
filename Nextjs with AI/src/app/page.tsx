7bf2a0b7e39abb79b6bba1e08d9f74a4d025f055
Muhammad Talha <143874100+talhak911@users.noreply.github.com>
2024-02-27 17:16:59 +0500

    Update page.tsx

--- Nextjs with AI/src/app/page.tsx
+++ Nextjs with AI/src/app/page.tsx
@@ -24,7 +24,7 @@ finally{
 	const response = await fetch(
     "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0",
 		{
-			headers: { Authorization: "Bearer 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" },
+			headers: { Authorization: "Bearer" },
 			method: "POST",
 			body: JSON.stringify(data),
 		}
