fb40479eaa37c87125a37e8c782e3db4b49970cd
talhak911 <talhakhan8300@gmail.com>
2024-03-12 16:40:30 +0500

    fetch post method

---
+++ NextJs backend/src/app/api/signin/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export async function POST (req){
+    console.log(req)
+    const body= await req.json()
+    return NextResponse.json({message:`your email is ${body.email}`})
+
+}
