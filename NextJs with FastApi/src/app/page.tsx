0b58c8299c3308460c8c5425cec1edee2d4312bd
talhak911 <talhakhan8300@gmail.com>
2024-04-02 15:11:35 +0500

    nextjs with fastapi

---
+++ NextJs with FastApi/src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Page(){
+  return (
+   
+     <dialog >
+ <div className=" bg-fuchsia-200 max-w-2xl mx-auto mt-20">
+      <form className="flex flex-col p-3">
+           <label htmlFor="">Name</label>
+            <input type="text"/>
+      </form>
+      </div>
+     </dialog>
+    
+  )
+}
